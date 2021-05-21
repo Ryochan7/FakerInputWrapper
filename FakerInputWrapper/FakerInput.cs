c4ca355f04880908423a47b550417efcdca45a4c
Travis Nickles <ryochan7@protonmail.com>
2021-05-20 21:06:12 -0500

    Changed C++ function name for abs mouse

--- FakerInputWrapper/FakerInput.cs
+++ FakerInputWrapper/FakerInput.cs
@@ -32,7 +32,7 @@ namespace FakerInputWrapper
             short x, short y, byte wheelPosition, byte hWheelPosition);
 
         [DllImport("FakerInputDll.dll")]
-        public static extern bool fakerinput_update_mouse(PFAKERINPUT_HANDLE clientHandle, byte button, ushort x, ushort y,
+        public static extern bool fakerinput_update_absolute_mouse(PFAKERINPUT_HANDLE clientHandle, byte button, ushort x, ushort y,
             byte wheelPosition, byte hWheelPosition);
 
 
@@ -117,7 +117,7 @@ namespace FakerInputWrapper
         {
             if (connected)
             {
-                return fakerinput_update_mouse(deviceHandle, (byte)report.Buttons,
+                return fakerinput_update_absolute_mouse(deviceHandle, (byte)report.Buttons,
                     report.MouseX, report.MouseY, report.WheelPosition, report.HWheelPosition);
             }
             else
